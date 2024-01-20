5d3de19014be6cf8bb039caf6e95f5a204f8e34e
tahmim al ashik <tahmim0006@gmail.com>
2024-01-21 00:55:28 +0600

    ontroller at api.php , create route group for add products with route resource in api.php. Added success and unsuccessful response function is base controller."

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    //
+     /**
+     * return success
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success'=> true,
+            'data'=>$result,
+            'message'=>$message,
+        ]
+        return response()->json($response, 200);
+    }
+
+    /**
+     * return error
+     */
+
+     public function sendError($error,$errorMessage=[], $code=404){
+        $response=[
+            'success'=>false,
+            'message'=>$error,
+
+        ];
+        if(!empty($errorMessage)){
+            $response['data']=>$errorMessage;
+        }
+        return response()->json($response, $code);
+
+     }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', [RegisterController::class,'register']);
+Route::post('login', [RegisterController::class,'login']);
+
+Route::group(['middleware'=>'auth:sanctum'],function(){
+
+    //ProductController
+    Route::resource('products', ProductController::class);
 });
